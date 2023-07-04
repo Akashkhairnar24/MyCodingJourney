b334cba87e37228069ece09cf85a44e794b1f867
Akashkhairnar24 <137057113+Akashkhairnar24@users.noreply.github.com>
2023-07-04 12:35:14 +0530

    Add files via upload

---
+++ First_Occurence.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int first_occurence(vector<int> &v,int n,int target)
+{
+    int low=0,high=n-1,mid,mini=INT_MAX;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        if(v[mid]==target)  
+        {
+            if(mid<mini)
+            mini = mid;
+            high = mid-1;
+        }
+        else if(target>v[mid])
+        {
+            low = mid+1;
+        }
+        else
+        high = mid-1;
+    }
+    if(mini>=0)
+    return mini;
+    else
+    return -1;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    int target;
+    cin>>target;
+    cout<<first_occurence(v,n,target)<<"\n";
+    return 0;
+}
+
+// TC => O(logN)
+// SC => O(1)   
